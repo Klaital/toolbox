60a290e6d3c735664f64c425a5f4ddcc7fe58208
Chris Cox <chriscox@abandonedfactory.net>
2013-12-09 14:56:33 -0800

    Adding another function to collect the 'stray', like how you can list multiple files to rm

--- lib/parse_arg.rb
+++ lib/parse_arg.rb
@@ -17,3 +17,16 @@ def parse_arg(args, patterns, expect_value=false, default_value=nil)
   
   return default_value
 end
+
+# Return an array containing all 'unaffiliated' arguments, 
+# meaning they are not preceeded by a parameter starting with '-'
+def parse_extra_args(args)
+  args[0..-2].each_index do |i|
+    if (args[i].start_with?('-'))
+      # Delete the flag, and its value if present
+      args.delete_at(i+1) unless(args[i+1].nil? || args[i+1].start_with?('-'))
+      args.delete_at(i)
+    end
+  end
+  return args
+end
