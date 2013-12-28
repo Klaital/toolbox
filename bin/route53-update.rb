a50c2a9cad5551debde3665844899fed2e80f71f
Chris Cox <chriscox@abandonedfactory.net>
2013-12-28 11:25:01 -0800

    Bugfixes after live test

--- bin/route53-update.rb
+++ bin/route53-update.rb
@@ -13,7 +13,7 @@ def find_cname_for_domain(domain)
         puts "[#{hz.id}]\tRsetType\t#{rset.type}"
         puts "[#{hz.id}]\tRSetID\t#{rset.set_identifier}"
         puts "[#{hz.id}]\tRSetRR\t#{rset.resource_records}"
-        puts "[#{hz.id}]\tValue\t#{rset.resource_records[:value]}"
+        puts "[#{hz.id}]\tValue\t#{rset.resource_records.to_s}"
         domain_rset = rset
       end
     end
@@ -34,6 +34,7 @@ The IP address is pulled from ipecho.net
 If you wish to override this default name, specify it as the second command line parameter, like this:
 > ruby route53-update.rb 3pg-enlistener.tmus.info ec2-107-20-108-70.compute-1.amazonaws.com
 USAGE
+end
 
 if (ARGV.length == 0)
   puts usage
@@ -44,7 +45,7 @@ domain =  ARGV[0]
 new_value = if(ARGV.length >= 2)
   ARGV[1]
 else
-  `\curl ipecho.net/plain`.strip
+  `curl ipecho.net/plain`.strip
 end
 
 # These records have a trailing '.' character for some reason.
@@ -54,8 +55,9 @@ puts "Searching for Record Sets with name matching '#{domain}'"
 rset = find_cname_for_domain(domain)
 puts "#{rset}"
 
-if (!rset.nil && rset.type == 'CNAME')
-  rset.resource_records[:value] = new_value
+if (!rset.nil? && rset.type == 'CNAME')
+  puts "Updating CNAME with new value: #{new_value}"
+  rset.resource_recordsi[0][:value] = new_value
   rset.update
 end
 
