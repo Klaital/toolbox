b58166c21c7532bea6905c272ac998d6305edc01
Chris Cox <chriscox@abandonedfactory.net>
2014-01-24 10:52:06 -0800

    Loading credentials from the config file

--- lib/configs_aws.rb
+++ lib/configs_aws.rb
@@ -1,5 +1,6 @@
 # Configure the AWS settings
 require 'aws-sdk'
+require_relative '../lib/configs'
 
-AWS.config( {:access_key_id => '', :secret_access_key => ''} )
+AWS.config( {:access_key_id => CREDS[:aws][:access_key], :secret_access_key => CREDS[:aws][:secret_key]} )
 
